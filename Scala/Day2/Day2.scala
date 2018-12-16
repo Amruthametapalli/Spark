d97b62fa3531ba555d003d53330086684a3c875e
Amruthametapalli <45731034+Amruthametapalli@users.noreply.github.com>
2018-12-16 11:34:53 -0800

    Update Day2.scala

--- Scala/Day2/Day2.scala
+++ Scala/Day2/Day2.scala
@@ -1,20 +1,20 @@
-//Reading json data from path
+## Reading json data from path
 val flightDF = spark.read.json("/databricks-datasets/definitive-guide/data/flight-data/json/")
 
-//Converting json to parquet and storing it in path
+## Converting json to parquet and storing it in path
 flightDF.write.parquet("/mnt/Amrutha/day2/json_to_parquet_conversion")
 
-//Reading the stored parquet data
+## Reading the stored parquet data
 val resultDF = spark.read.parquet("/mnt/Amrutha/day2/json_to_parquet_conversion")
 
-//Validating source and target data
+## Validating source and target data
 val diff = flightDF.except(resultDF)
 display(diff)
 
-//Reading and checking the number of columns
+## Reading and checking the number of columns
 val resultColNum = resultDF.columns.size
 assert (resultColNum == 3, "Target Column size != 3")
 
-//Validating if type of count column is long
+## Validating if type of count column is long
 val colType = resultDF.schema("count").dataType
 assert(colType == org.apache.spark.sql.types.DataTypes.LongType, "Column type is not Long")
